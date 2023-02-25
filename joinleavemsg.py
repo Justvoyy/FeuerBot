439e1cbe814bf242b2087fcaefb6b7a85fbae4e6
EinfachNurHase <124504748+EinfachNurHase@users.noreply.github.com>
2023-02-25 12:04:10 +0100

    Create joinleavemsg.py

---
+++ joinleavemsg.py
@@ -0,0 +1,17 @@
+@client.event
+async def on_member_join(member):
+    guild = member.guild
+    stat = client.get_channel(1073291839806898287)
+    await stat.edit(name = f"Members: {guild.member_count}")
+    channel = client.get_channel(760549039602925598)
+    await channel.send(f"Hey {member.mention}! Welcome on ...")
+    await user_log.info(f"{member} ist dem Server gejoined")
+  
+@client.event
+async def on_member_remove(member):
+    guild = member.guild
+    stat = client.get_channel(1073291839806898287)
+    await stat.edit(name = f"Mitglieder: {guild.member_count}")
+    channel = client.get_channel(760549099656314920)
+    await channel.send(f"{member} left the server!")
+    await user_log.info(f"{member} hat den Server verlassen")
