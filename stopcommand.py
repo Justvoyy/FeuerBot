7d95ca0c4269abfc4f8df509546e83eb6b2af2e4
EinfachNurHase <124504748+EinfachNurHase@users.noreply.github.com>
2023-03-01 14:47:08 +0100

    English Translation

--- stopcommand.py
+++ stopcommand.py
@@ -2,8 +2,8 @@
 @has_permissions(administrator=True)
 @app_commands.guild_only()
 async def stopcommand(int:discord.Interaction):
-  await int.response.send_message("Der Bot wird beendet.")
-  user_log.info(f"Bot wurde durch {int.user} beendet")
+  await int.response.send_message("Bot will be closed")
+  user_log.info(f"Bot got closed by {int.user}")
   log = client.get_channel(1066754831303323648)
-  await log.send(f"Bot wurde durch {int.user} beendet")
+  await log.send(f"Bot got closed by {int.user}")
   await client.close()
