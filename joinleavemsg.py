19a56d47e49a1cd5a070d559c7f9cd5aa0f59df3
EinfachNurHase <124504748+EinfachNurHase@users.noreply.github.com>
2023-03-04 17:03:16 +0100

    Translation

--- joinleavemsg.py
+++ joinleavemsg.py
@@ -5,7 +5,7 @@ async def on_member_join(member):
     await stat.edit(name = f"Members: {guild.member_count}")
     channel = client.get_channel(760549039602925598)
     await channel.send(f"Hey {member.mention}! Welcome on ...")
-    await user_log.info(f"{member} ist dem Server gejoined")
+    await user_log.info(f"{member} joined the server!")
   
 @client.event
 async def on_member_remove(member):
@@ -14,4 +14,4 @@ async def on_member_remove(member):
     await stat.edit(name = f"Mitglieder: {guild.member_count}")
     channel = client.get_channel(760549099656314920)
     await channel.send(f"{member} left the server!")
-    await user_log.info(f"{member} hat den Server verlassen")
+    await user_log.info(f"{member} left the server!")
